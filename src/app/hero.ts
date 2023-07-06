571dca96c76830f8e27312973f30c1901be82d5a
andresdlRoca <andresdelaroca11@gmail.com>
2023-07-06 13:14:37 -0600

    Added mockup heroes, added hero list and hero details components

---
+++ src/app/hero.ts
@@ -0,0 +1,6 @@
+export interface Hero {
+    id: number;
+    superpower: string;
+    name: string;
+    ranking: number;
+}
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,16 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 1, superpower: 'Super Strength', name: 'Mr. Incredible', ranking: 50 },
+    { id: 2, superpower: 'Super Speed', name: 'Dash', ranking: 40 },
+    { id: 3, superpower: 'Invisibility', name: 'Violet', ranking: 30 },
+    { id: 4, superpower: 'Elasticity', name: 'Elastigirl', ranking: 20 },
+    { id: 5, superpower: 'One for All', name: 'All Might', ranking: 1 },
+    { id: 6, superpower: 'Explosion', name: 'Bakugo', ranking: 10 },
+    { id: 7, superpower: 'Half-Cold Half-Hot', name: 'Todoroki', ranking: 5 },
+    { id: 8, superpower: 'Engine', name: 'Iida', ranking: 15 },
+    { id: 9, superpower: 'Creation', name: 'Momo', ranking: 25 },
+    { id: 10, superpower: 'Zero Gravity', name: 'Uraraka', ranking: 35 },
+    { id: 11, superpower: 'Dark Shadow', name: 'Tokoyami', ranking: 5 }
+];
+
